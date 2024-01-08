08ca33928420d047b9b7b6b1e8da35b4e39aca15
P_Janardhan <pjanardhan7u@gmail.com>
2024-01-08 23:14:14 +0530

    implemented theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
-import React from 'react' 
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import React from "react";
 import { Inter, Space_Grotesk } from "next/font/google";
-import type {Metadata} from 'next';
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
-const inter=Inter({
-  subsets:['latin'],
+const inter = Inter({
+  subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter",
-})
+});
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -16,39 +17,33 @@ const spaceGrotesk = Space_Grotesk({
   variable: "--font-spaceGrotesk",
 });
 
-
-
-
-export const metadata:Metadata={
-  title:"DevOverflow",
-  description:"A comunity driven platform for asking and solving doubts",
-  icons:{
-    icon:'/assets/images/site-logo.svg'
-  }
-}
-
-
+export const metadata: Metadata = {
+  title: "DevOverflow",
+  description: "A comunity driven platform for asking and solving doubts",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-    appearance={{
-      elements: {
-        formButtonPrimary: "primary-gradient",
-        footerActionLink: "primary-text-gradient hover:text-primary-500",
-      },
-    }}
-    >
-      <html lang="en">
-        
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-        
-        {children}</body>
-      </html>
-    </ClerkProvider>
-  )
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
