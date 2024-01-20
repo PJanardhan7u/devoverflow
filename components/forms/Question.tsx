0e17cb00bfe8b30a42e57f8c60331738d6dcb009
P_Janardhan <pjanardhan7u@gmail.com>
2024-01-20 14:38:44 +0530

    tags functionality is implemented

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -17,8 +17,16 @@ import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
 import React, { useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
+import { Badge } from "../ui/badge";
+import Image from "next/image";
 
-const Question = () => {
+interface Props {
+  type?: string;
+  mongoUserId: string;
+  questionDetails?: string;
+}
+
+const Question = ({ type, mongoUserId, questionDetails }: Props) => {
   const editorRef = useRef(null);
 
   const log = () => {
@@ -42,6 +50,42 @@ const Question = () => {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+
+
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag must be less than 15 characters.",
+          });
+        }
+
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        }
+      } else {
+        form.trigger();
+      }
+
+      }
+  }
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+
+    form.setValue("tags", newTags);
+  };
 
   return (
     <Form {...form}>
@@ -82,13 +126,12 @@ const Question = () => {
                 <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-
                 <Editor
                   apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
-                  
-                  onInit={(evt, editor) => 
+                  onInit={(evt, editor) =>
                     // @ts-ignore
-                    (editorRef.current = editor)}
+                    (editorRef.current = editor)
+                  }
                   initialValue="<p>Explanation</p>"
                   init={{
                     height: 350,
@@ -114,11 +157,9 @@ const Question = () => {
                       "undo redo | " +
                       "codesample | bold italic forecolor | alignleft aligncenter |" +
                       "alignright alignjustify | bullist numlist",
-                      content_style: "body { font-family:Inter; font-size:16px }",
-                      
+                    content_style: "body { font-family:Inter; font-size:16px }",
                   }}
                 />
-       
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Be specific and imagine you&apos;re asking a question to another
@@ -131,17 +172,49 @@ const Question = () => {
 
         <FormField
           control={form.control}
-          name="title"
+          name="tags"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col">
               <FormLabel className="paragraph-semibold text-dark400_light800">
                 Question Title <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  {...field}
-                />
+                <div>
+                  
+                  <Input
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    placeholder="Add tags..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                   {field.value.length > 0 && (
+                      <div className="flex-start mt-2.5 gap-2.5">
+                        {field.value.map((tag: any) => (
+                          <Badge
+                            key={tag}
+                            className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                            onClick={() =>
+                              type !== "Edit"
+                                ? handleTagRemove(tag, field)
+                                : () => {}
+                            }
+                          >
+                            {tag}
+                            {type !== "Edit" && (
+                              <Image
+                                src="/assets/icons/close.svg"
+                                alt="Close icon"
+                                width={12}
+                                height={12}
+                                className="cursor-pointer object-contain invert-0 dark:invert"
+                              />
+                            )}
+                          </Badge>
+                        ))}
+                      </div>
+                    )}
+                </div>
+
+
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Add up to 3 tags to describe what your question is about. You
@@ -152,7 +225,12 @@ const Question = () => {
           )}
         />
 
-        <Button type="submit">Submit</Button>
+        <Button
+          type="submit"
+          className="primary-gradient w-fit !text-light-900"
+        >
+          Submit
+        </Button>
       </form>
     </Form>
   );
