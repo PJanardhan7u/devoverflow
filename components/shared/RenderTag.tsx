0995a1fc949748b279f2bdc7f88b73580fed4f41
P_Janardhan <pjanardhan7u@gmail.com>
2024-01-21 20:31:20 +0530

    updated type error

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Link from 'next/link'
 import { Badge } from "@/components/ui/badge";
 
-interface Props{
-    _id:string;
-    name:string;
-    totalQuestions:number;
-    showCount:boolean;
+interface Props {
+  _id: string;
+  name: string;
+  totalQuestions?: number;
+  showCount?: boolean;
 }
 
-const RenderTag = ({_id,name,totalQuestions,showCount}) => {
+const RenderTag = ({_id,name,totalQuestions,showCount}:Props) => {
   return (
     <div>
         <Link href={`/tags/${_id}`} className="flex justify-between gap-2">
