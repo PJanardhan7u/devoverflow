a6074a163a01c9f1f662e673fde38ae42b5a6b74
P_Janardhan <pjanardhan7u@gmail.com>
2024-01-21 12:00:55 +0530

    mongo db connected nowneed to implement dummy user

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,17 +1,45 @@
-"use server"
+"use server";
+
+import { connectToDatabase } from "../mongoose";
+import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
 
 
-import { connectToDatabase } from "../mongoose"
 
+export async function createQuestion(params: any) {
+  try {
+    // connect db
+    connectToDatabase();
+    const { title, content, tags, author, path } = params;
 
+    // create question
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+    const tagDocuments=[]
 
-export async function createQuestion() {
-    try{
-        // connect db
-        connectToDatabase()
+    // create tags 
+    for (const tag of tags) {
+        const existingTag = await Tag.findOneAndUpdate(
+          { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+          { $setOnInsert: { name: tag }, $push: { questions: question._id } },
+          { upsert: true, new: true }
+        );
+  
+        tagDocuments.push(existingTag._id);
+      } 
+      await Question.findByIdAndUpdate(question._id, {
+        $push: { tags: { $each: tagDocuments } },
+      });
 
-    }
-    catch(error){
+    //   create an interation record for users ask question
 
-    }
+    // increment user's reputation by 5
+
+  } catch (error) {
+
+  }
+
 }
